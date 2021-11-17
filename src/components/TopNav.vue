2b22467f9b7c5bbe5e747a1f151b09983c00a273
MR17009 <MR17009@ues.edu.sv>
2021-11-17 17:22:58 -0600

    creacion navbar funcional

---
+++ src/components/TopNav.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-toolbar-titlte>
+        <v-icon>mdi-store</v-icon>Cellphone Store</v-toolbar-titlte
+      >
+
+      <v-spacer></v-spacer>
+
+      <span class="hidden-sm-and-up">
+        <v-btn @click.stop="drawer = !drawer"><v-icon>mdi-menu</v-icon></v-btn>
+      </span>
+
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn to="/" text> Inicio </v-btn>
+        <v-btn to="/" text> Estadisticas </v-btn>
+        <v-btn to="/" text>
+          <v-icon right>mdi-plus</v-icon>
+        </v-btn>
+        <v-btn to="/" text>
+          <v-icon right>mdi-cart</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      <v-list >
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>mdi-{{ item.icon}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+module.exports = {
+  data() {
+    return {
+      drawer: false,
+      items: [
+          { title: "Home", link: "/", icon: "home" },
+          { title: "Estadisticas", link: "/", icon: "#" },
+          { title: "Nuevo", link: "/", icon: "plus" },
+          { title: "Carrito", link: "/", icon: "cart" }
+      
+      ],
+    };
+  },
+};
+</script>
