4c82f7e2e5351359d4459782b0bb6905781b7aec
MR17009 <MR17009@ues.edu.sv>
2021-11-17 20:38:10 -0600

    esqueleto main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row>
+      <v-col sm="3" offset-lg="1">
+        sidebar
+      </v-col>
+      <v-col sm="9" lg="7">
+        productos
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
 </script>
