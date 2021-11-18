21af49843cfced96fd4473d4d246fdb05ac62251
MR17009 <MR17009@ues.edu.sv>
2021-11-18 10:30:46 -0600

    conectar con firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
+
+
+
+var firebaseConfig={
+    apiKey: "AIzaSyCKE_4Dbz7bOlKdKBd46naDc2-zKtSp2T8",
+  authDomain: "mr-store-da0b5.firebaseapp.com",
+  projectId: "mr-store-da0b5",
+  storageBucket: "mr-store-da0b5.appspot.com",
+  messagingSenderId: "1040550248552",
+  appId: "1:1040550248552:web:52a077443e54eec3fb7bdd",
+  measurementId: "G-JJVQNWW81Z"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
