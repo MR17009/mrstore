f3018c495e414243424bcc251453971597b3ef38
MR17009 <MR17009@ues.edu.sv>
2021-11-19 20:00:34 -0600

    search bug eliminado

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -4,23 +4,19 @@
       <v-toolbar-titlte>
         <v-icon>mdi-store</v-icon>Cellphone Store</v-toolbar-titlte
       >
-      
+
       <v-spacer></v-spacer>
 
       <span class="hidden-sm-and-up">
         <v-btn @click.stop="drawer = !drawer"><v-icon>mdi-menu</v-icon></v-btn>
       </span>
-      <v-toolbar-items>
-        <v-icon>mdi-magnify</v-icon>
-        <input @filtarAnuncios="filtrarAnuncios" v-in:keyup.enter>
-      </v-toolbar-items>
       <v-toolbar-items class="hidden-xs-only">
         <v-btn to="/" text> Inicio </v-btn>
         <v-btn to="/" text>
-        <v-icon left>mdi-chart-bar</v-icon>Estadisticas          
+          <v-icon left>mdi-chart-bar</v-icon>Estadisticas
         </v-btn>
         <v-btn to="/" text>
-          <v-icon >mdi-plus</v-icon>
+          <v-icon>mdi-plus</v-icon>
         </v-btn>
         <v-btn to="/" text>
           <v-icon>mdi-cart</v-icon>
@@ -28,10 +24,10 @@
       </v-toolbar-items>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" absolute temporary right>
-      <v-list >
+      <v-list>
         <v-list-item v-for="item in items" :key="item.title" link>
           <v-list-item-icon>
-            <v-icon>mdi-{{item.icon}}</v-icon>
+            <v-icon>mdi-{{ item.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
@@ -42,19 +38,18 @@
   </div>
 </template>
 <script>
-import Buscar from './Buscar.vue' 
- export default {
-  components: { },
+import Buscar from "./Buscar.vue";
+export default {
+  components: {},
   data() {
     return {
       drawer: false,
       Buscar,
       items: [
-          { title: "Home", link: "../src/views/Store.vue", icon: "home" },
-          { title: "Estadisticas", link: "/", icon: "chart-bar" },
-          { title: "Nuevo", link: "/", icon: "plus" },
-          { title: "Carrito", link: "/", icon: "cart" }
-      
+        { title: "Home", link: "../src/views/Store.vue", icon: "home" },
+        { title: "Estadisticas", link: "/", icon: "chart-bar" },
+        { title: "Nuevo", link: "/", icon: "plus" },
+        { title: "Carrito", link: "/", icon: "cart" },
       ],
     };
   },
